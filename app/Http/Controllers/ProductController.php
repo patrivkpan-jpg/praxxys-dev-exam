0b867c1bd64def4db0753d11c62053591ea50b86
patrivkpan-jpg <patrivkpan98@gmail.com>
2024-05-10 20:59:27 +0800

    Remove used php files and code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -95,14 +95,6 @@ class ProductController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(Product $product)
-    {
-
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
     public function edit(int $id)
--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [AuthenticatedSessionController::class, 'index'])
     ->name('index');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 Route::get('/products/get', [ProductController::class, 'get'])->name('product.get');
 Route::post('/products/validate', [ProductController::class, 'validate'])->name('product.validate');
-Route::resource('products', ProductController::class)->names([
-    'index' => 'product.index',
-    'create' => 'product.create',
-    'store' => 'product.store',
-    'edit' => 'product.edit',
-    'update' => 'product.update',
-    'destroy' => 'product.destroy'
-]);;
+Route::resource('products', ProductController::class)
+    ->except(['show'])
+    ->names([
+        'index' => 'product.index',
+        'create' => 'product.create',
+        'store' => 'product.store',
+        'edit' => 'product.edit',
+        'update' => 'product.update',
+        'destroy' => 'product.destroy'
+    ]);
 
 require __DIR__.'/auth.php';
